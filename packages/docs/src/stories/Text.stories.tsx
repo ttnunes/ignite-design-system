1d188e07d0b2ed203b7cd54c1e6447a10c24bda8
Thiago Teixeira Nunes <thiagonunes.cx@gmail.com>
2022-11-15 12:27:58 -0300

    add core design system

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nobis laborum in maxime modi possimus ducimus recusandae consequuntur, aperiam delectus corrupti quidem atque. Error, sunt iste fugit voluptatum exercitationem earum.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o text sempre será um `p`, mas podemos alterar isso com a propriedade `as`',
+      },
+    },
+  },
+}
